bc59962381871156b5855da3e8ec4a09eab68795
Zanderwohl <zandy@zanderlowry.com>
2022-03-24 19:28:37 -0500

    Basic box

--- terminal.c
+++ terminal.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <curses.h>
 
 WINDOW *mainwindow;
 
 int main()
 {
-  mainwindow = initscr(); // Start curses.
-  printf("Hello, world!\n");
+  // Set up
+  if((mainwindow = initscr()) == NULL) // Start curses.
+  {
+    printf("Error initilizing curses.");
+    exit(1);
+  }
+
+  // Do stuff
+  box(mainwindow, ACS_VLINE, ACS_HLINE);
+
+  // Display
+  refresh();
+
+  // Show for a second
+  sleep(3);
+
+  // Clean Up
+  delwin(mainwindow);
   endwin(); // Stop curses, to restore shell to normal behavior.
+  refresh();
+
+  return 0;
 }
