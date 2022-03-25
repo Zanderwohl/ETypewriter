7b80a8ea9e5f650043b8d6d09cb0616e74bf724e
Zanderwohl <zandy@zanderlowry.com>
2022-03-24 22:36:00 -0500

    Move key status to top right

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # ETypewriter
 
 ETypewriter is a shell for simple word processing and file management from the command line.
+
+## Using
+
+Run
+
+```
+make && ./terminal.o
+```
+
--- terminal.c
+++ terminal.c
@@ -6,8 +6,13 @@
 
 #define ESC 27
 
+// Main window
 WINDOW *mainwindow;
+
+// The keypress this loop.
 int keypress;
+
+int max_x, max_y;
 
 int main()
 {
@@ -17,7 +22,9 @@ int main()
     printf("Error initilizing curses.");
     exit(1);
   }
-  noecho();
+  noecho(); // Turn off character echo
+  wmove(mainwindow, 1, 1);
+  getmaxyx(mainwindow, max_y, max_x);
 
   // Do stuff
   box(mainwindow, ACS_VLINE, ACS_HLINE);
@@ -25,9 +32,11 @@ int main()
   // Display
   refresh();
 
+  int key_press_count = 0;
   // Main loop
   while ((keypress = getch()) != ESC)
   {
+    mvprintw(1, max_x - 3, "%i", ++key_press_count);
     //deleteln();
     mvprintw(1, 1, "%x", keypress);
     refresh();
